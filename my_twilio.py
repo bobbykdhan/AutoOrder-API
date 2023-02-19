2dbbc2fceae49f4266f6bcc63ea9143a0110a7eb
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2023-02-19 17:03:41 -0500

    Put all files related to sending texts in one file

---
+++ my_twilio.py
@@ -0,0 +1,33 @@
+import os
+
+from twilio.rest import Client
+
+
+def send_text(phone_number, message):
+    if int(os.environ['DEBUG']):
+        auth_token = os.environ['TWILIO_TEST_TOKEN']
+    else:
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body=message,
+        from_='+1' + os.environ['TWILIO_PHONE_NUMBER'],
+        to='+1' + phone_number
+    )
+    return message.sid
+
+def handle_message():
+    pass
+
+def cancel_order(driver):
+    pass
+
+def failed_order(driver,reason):
+    pass
+
+def repeat_order(driver):
+    pass
