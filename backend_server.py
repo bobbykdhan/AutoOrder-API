887b10c775c3a94ba010cb3888cefb14bf0f2e96
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2023-02-20 03:17:26 -0500

    Commented the start text message

--- backend_server.py
+++ backend_server.py
@@ -1,17 +1,15 @@
-import os
 import time
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-import my_twilio
 from image import upload_screenshot
 from webdriver_handler import *
 
 load_dotenv()
 
 app = FastAPI()
-my_twilio.send_text("Personal Order server started", os.getenv("PHONE_NUMBER"))
+# my_twilio.send_text("Personal Order server started", os.getenv("PHONE_NUMBER"))
 
 
 @app.get("/")
