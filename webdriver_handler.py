72ca061ce12fa204af88ae950751c798b454154e
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2023-02-24 22:41:53 -0500

    Added support for using remote webdrivers

--- webdriver_handler.py
+++ webdriver_handler.py
@@ -1,5 +1,3 @@
-import os
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -7,14 +5,30 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-def create_driver(firefox=False, headless=True, screen_size=(3000, 3000)):
-    os.umask(0)
+def create_driver(url="http://localhost:4444", firefox=True, screen_size=(3000, 3000)):
     if firefox:
-        #service = FirefoxService(GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+    else:
+        options = webdriver.ChromeOptions()
+
+    options.add_argument("--window-size=%s,%s" % screen_size)
+    if firefox:
+        return webdriver.Remote(
+            command_executor=url,
+            options=options
+        )
+    else:
+        return webdriver.Remote(
+            command_executor=url,
+            options=options
+        )
+
+
+def create_local_driver(firefox=False, headless=False, screen_size=(3000, 3000)):
+    if firefox:
         service = FirefoxService(GeckoDriverManager(path=r"drivers").install())
         options = webdriver.FirefoxOptions()
     else:
-        # service = ChromeService(ChromeDriverManager(path=r"drivers").install())
         service = ChromeService(ChromeDriverManager(path=r"drivers").install())
         options = webdriver.ChromeOptions()
 
